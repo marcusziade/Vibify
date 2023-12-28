37f41b4dd1b7fbc25db6fb8d4b0366e86a617ce5
Marcus Ziadé <marcus.ziade@ouraring.com>
2023-12-27 19:01:59 -0800

    Initial Commit

---
+++ Vibify/VibifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  VibifyApp.swift
+//  Vibify
+//
+//  Created by Marcus Ziadé on 27.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct VibifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
