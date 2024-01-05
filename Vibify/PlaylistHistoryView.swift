db0979d402ca1ac4dd59d8bcdb48c0a15c7cce75
Marcus Ziadé <marcus.ziade@ouraring.com>
2024-01-04 18:00:46 -0800

    Fix database inserting not inserting songs

--- Vibify/PlaylistHistoryView.swift
+++ Vibify/PlaylistHistoryView.swift
@@ -11,7 +11,7 @@ struct PlaylistHistoryView: View {
                 Text(playlist.title)
                 Text(playlist.playlistID)
                 Text(playlist.createdAt.formatted())
-                Text(String(playlist.duration))
+                Text(String(playlist.duration.formatted()))
             }
         }
     }
