ad68fc1d30f22199bd346421b6c6d8c1b03440d8
Marcus Ziadé <marcus.ziade@ouraring.com>
2024-01-20 18:33:26 -0800

    Add DalleGenerator and initialize main view in App root

--- Vibify/API/Dalle/DalleRequest.swift
+++ Vibify/API/Dalle/DalleRequest.swift
@@ -3,6 +3,15 @@ import Foundation
 struct DalleRequest: Codable {
     let model: String
     let prompt: String
-    let n: Int
-    let size: String
+    let n: Int?
+    let size: String?
+    let quality: String?
+    let responseFormat: String?
+    let style: String?
+    let user: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case model, prompt, n, size, quality, style, user
+        case responseFormat = "response_format"
+    }
 }
