e627b190319f59920e4e504682befeab86ede83d
Marcus Ziadé <marcus.ziade@ouraring.com>
2024-02-03 16:10:59 -0800

    Update default value for `detail` parameter in `VisionRequest` struct to "auto"

--- Vibify/API/Vision/VisionRequest.swift
+++ Vibify/API/Vision/VisionRequest.swift
@@ -19,7 +19,10 @@ struct VisionRequest: Codable {
             let text: String? = Prompts.visionPrompt
             let imageURL: URL? = nil
             let base64Image: String?
-            let detail: String? = "low"
+            /// By controlling the detail parameter, which has three options, `low`, `high`, or `auto`,
+            /// you have control over how the model processes the image and generates its textual understanding.
+            /// By default, the model will use the `auto` setting which will look at the image input size and decide if it should use the `low` or `high` setting.
+            let detail: String? = "auto"
             
             enum CodingKeys: String, CodingKey {
                 case type, text
