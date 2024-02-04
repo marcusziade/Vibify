9bc58f074141f92e29635d1e3a79d5c52fa55ffa
Marcus Ziadé <marcus.ziade@ouraring.com>
2024-02-03 16:52:08 -0800

    Add Tuist files and plugins

---
+++ tuist/Project.swift
@@ -0,0 +1,31 @@
+import MyPlugin
+import ProjectDescription
+import ProjectDescriptionHelpers
+
+/*
+                +-------------+
+                |             |
+                |     App     | Contains Tuist App target and Tuist unit-test target
+                |             |
+         +------+-------------+-------+
+         |         depends on         |
+         |                            |
+ +----v-----+                   +-----v-----+
+ |          |                   |           |
+ |   Kit    |                   |     UI    |   Two independent frameworks to share code and start modularising your app
+ |          |                   |           |
+ +----------+                   +-----------+
+
+ */
+
+// MARK: - Project
+
+// Local plugin loaded
+let localHelper = LocalHelper(name: "MyPlugin")
+
+// Creates our project using a helper function defined in ProjectDescriptionHelpers
+let project = Project.app(
+    name: "Tuist",
+    destinations: .iOS,
+    additionalTargets: ["TuistKit", "TuistUI"]
+)
